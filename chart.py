8a72b2a98cfa0672c7a4151d5034cebdfc73eab9
mdzapeer <mmudassers@gmail.com>
2018-05-30 02:24:59 +0400

    Added BTC chart

---
+++ chart.py
@@ -0,0 +1,29 @@
+
+import pandas
+from alpha_vantage.cryptocurrencies import CryptoCurrencies
+from bokeh.plotting import figure, show , output_file
+from bokeh.embed import components
+
+cc = CryptoCurrencies(key='K53QQNRE6X9S2YCI', output_format='pandas')
+
+data, meta_data = cc.get_digital_currency_daily(symbol='BTC', market='USD')
+
+data["Date"]=pandas.to_datetime(data.index)
+data1=data[-50:]
+
+p=figure(x_axis_type='datetime', width=1000, height=300,responsive=True)
+p.title.text="Candlestick Chart"
+
+w = 12*60*60*1000 # half day in ms
+
+data1["Date"]=pandas.to_datetime(data1.index)
+
+increase=data1["4a. close (USD)"] > data1["1a. open (USD)"]
+decrease=data1["4a. close (USD)"] < data1["1a. open (USD)"]
+
+p.segment(data1.Date,data1["2b. high (USD)"],data1.Date,data1["3b. low (USD)"],color="black")
+
+p.vbar(data1.Date[increase],w,data1["1a. open (USD)"][increase],data1['4b. close (USD)'][increase],fill_color="green", line_color="black")
+p.vbar(data1.Date[decrease],w,data1["1a. open (USD)"][decrease],data1['4b. close (USD)'][decrease],fill_color="red", line_color="black")
+
+script1, div1 = components(p)
--- script1.py
+++ script1.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import chart
 
 app=Flask(__name__)
 
@@ -9,6 +10,10 @@ def home():
 @app.route('/about/')
 def about():
     return render_template("about.html")
+
+@app.route('/chart/')
+def chartpage():
+    return render_template("chart.html", script1=chart.script1, div1=chart.div1)
 
 if __name__=="__main__":
     app.run(debug=True)
